f33e436b191dccb549dd48907655fe52bdeb0088
Mason Raffo <Mason@Masons-MacBook-Air.local>
2018-05-19 17:38:13 -0400

    dummy ui almost done! :)

--- user_interface.py
+++ user_interface.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter
+from PIL import ImageTk, Image
 
 
 WINDOW_BG = "#ffffff"
@@ -12,6 +13,12 @@ DEFAULT_FONT_SIZE = 22
 COLOR_GOOD = "#15955f" # Green
 COLOR_WARN = "#c9bb00" # Yellow
 COLOR_BAD = "#ab0000" # Red
+
+
+
+# Image
+imagepath = 'sample chart.png'
+# icon = ImageTk.PhotoImage(Image.open(imagepath))
 
 
 class Window(Frame):
@@ -30,21 +37,25 @@ class Window(Frame):
         # self.box2Text = tkinter.Text(self.root, width=1, height=1)
         # self.box2Text.pack(fill=tkinter.BOTH,side=tkinter.RIGHT, expand=True)
 
-        self.root = tkinter.Tk()
+        self.root = master
         self.root.title("App")
         self.root.geometry("1000x500")
         self.root.config(bg = '#ffffff')
         self.root.update()
-        self.text = tkinter.Text(self.root, width=75, wrap=WORD, bg= '#ffffff')
-        self.text.pack(fill='y', side=tkinter.LEFT, expand=True)
+        self.labelframe = LabelFrame(self.root, text="", width=700, height= 1, bg= TEXTBOX_BG)
+        # self.box2Text = tkinter.Text(self.root, width=1, height=1)
+        # self.labelframe.pack(fill=tkinter.BOTH,side=tkinter.RIGHT, expand=True)
+        self.labelframe.pack(fill=tkinter.Y, side=tkinter.RIGHT, expand=False)
+
+
+        self.text = tkinter.Text(self.root, wrap=WORD, bg= '#ffffff', font=(DEFAULT_FONT, DEFAULT_FONT_SIZE), fg = "#777777")
+        # self.text.pack(fill=BOTH, side=tkinter.LEFT)
+        self.text.pack(fill=BOTH)
         # self.text.pack(fill=tkinter.BOTH,side=tkinter.LEFT, expand=True)
         # self.labelframe = LabelFrame(self.root, text="", width=1, height= 1, bg= TEXTBOX_BG)
 
 
-        self.labelframe = LabelFrame(self.root, text="", width=10000, height= 1, bg= TEXTBOX_BG)
-        # self.box2Text = tkinter.Text(self.root, width=1, height=1)
-        # self.labelframe.pack(fill=tkinter.BOTH,side=tkinter.RIGHT, expand=True)
-        self.labelframe.pack(fill=tkinter.BOTH,side=tkinter.RIGHT, expand=True)
+
 
         self.text.insert(END, '''Lorem ipsum de arbitrantur. Enim id ad quem offendit, ea quid quid quis excepteur eu cupidatat fugiat arbitror, qui nostrud distinguantur, nescius si labore ad id export dolore est nescius, hic ita quae doctrina est possumus id nostrud. Te appellat qui mentitum, eu velit constias.Quis consequat ab summis anim e veniam e excepteur. Anim ut nostrud ea summis, duis laborum eu vidisse. Admodum graviterque te ingeniis.
         #
@@ -80,19 +91,64 @@ class Window(Frame):
         self.volume_tip.pack(fill=tkinter.X, expand=False)
         self.speed_tip.pack(fill=tkinter.X, expand=False)
 
+        self.spaceholder1 = Frame(self.labelframe, bg = TEXTBOX_BG)
+        self.spaceholder1.pack(fill = X, pady= 50/2)
 
-        # Adding the charts for pace
+
+        # Frame for the pace chart!
+        self.labelframe_pacechart_numbers = Frame(self.labelframe, width=1, height= 1, bg = TEXTBOX_BG)
+        self.labelframe_pacechart_numbers.pack(fill=tkinter.X, expand=False , padx = 0)
+
+
+
+
+        self.pace_value_label = Label(self.labelframe_pacechart_numbers, text= "Pace:", fg="black", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE),  bg = TEXTBOX_BG)
+        self.pace_value = Label(self.labelframe_pacechart_numbers, text= "25 wpm", fg="black", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE),  bg = TEXTBOX_BG)
+
+
+
+
+        self.pace_value.pack(fill=tkinter.X, expand=True, side = RIGHT)
+        self.pace_value_label.pack(fill=tkinter.X, expand=True, side = LEFT)
+
         self.labelframe_pacechart = Frame(self.labelframe, width=1, height= 1, bg = TEXTBOX_BG)
-        self.labelframe_pacechart.pack(fill=tkinter.X, expand=False , padx = 10, pady = 50)
-
-        self.pace_value_label = Label(self.labelframe_pacechart, text= "Pace:", fg="black", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE),  bg = TEXTBOX_BG)
-        self.pace_value = Label(self.labelframe_pacechart, text= "25 wpm", fg="black", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE),  bg = TEXTBOX_BG)
 
 
+        self.photo = ImageTk.PhotoImage(Image.open('sample chart.png'))
+        self.Artwork = Label(self.labelframe_pacechart, image=self.photo)
+        self.Artwork.photo = self.photo
+        self.Artwork.pack()
+
+        self.labelframe_pacechart.pack(fill=tkinter.X, expand=False , padx = 0, pady = 0)
 
 
-        self.pace_value.pack(fill=tkinter.X, expand=False, side = RIGHT)
-        self.pace_value_label.pack(fill=tkinter.X, expand=False, side = LEFT)
+        self.spaceholder2 = Frame(self.labelframe, bg = TEXTBOX_BG)
+        self.spaceholder2.pack(fill = X, pady= 50/2)
+
+        self.labelframe_volumechart_numbers = Frame(self.labelframe, width=1, height= 1, bg = TEXTBOX_BG)
+        self.labelframe_volumechart_numbers.pack(fill=tkinter.X, expand=False , padx = 0)
+
+
+
+
+        self.volume_value_label = Label(self.labelframe_volumechart_numbers, text= "Volume:", fg="black", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE),  bg = TEXTBOX_BG)
+        self.volume_value = Label(self.labelframe_volumechart_numbers, text= "1.5x ambient", fg="black", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE),  bg = TEXTBOX_BG)
+
+
+
+
+        self.volume_value.pack(fill=tkinter.X, expand=True, side = RIGHT)
+        self.volume_value_label.pack(fill=tkinter.X, expand=True, side = LEFT)
+
+        self.labelframe_volumechart = Frame(self.labelframe, width=1, height= 1, bg = TEXTBOX_BG)
+
+
+        self.photo1 = ImageTk.PhotoImage(Image.open('sample chart.png'))
+        self.Artwork1 = Label(self.labelframe_volumechart, image=self.photo1)
+        self.Artwork1.photo = self.photo1
+        self.Artwork1.pack()
+
+        self.labelframe_volumechart.pack(fill=tkinter.X, expand=False , padx = 0, pady = 0)
 
 
         # self.bind("<Configure>", self.on_resize)
@@ -145,6 +201,8 @@ class Window(Frame):
     #     exit()
 
 root = Tk()
+# root = Toplevel()
+
 #
 # #size of the window
 #
