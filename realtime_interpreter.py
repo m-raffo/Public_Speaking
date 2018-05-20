bc13d5998bcda72798c51af388d595531669ab47
ethanalt <ethanjmedei@gmail.com>
2018-05-20 02:27:26 -0400

    jhi

--- realtime_interpreter.py
+++ realtime_interpreter.py
@@ -4,7 +4,7 @@ import os
 import re
 import sys
 import jellyfish
-import user_interface
+#import user_interface
 
 # Imports the Google Cloud client library
 import pyaudio
@@ -17,12 +17,13 @@ transcript_full = ""
 transcript_pending = ""
 wpm_current = 0
 
+scriptv = []
 # Instantiates a client
 # Audio recording parameters
 RATE = 16000
 CHUNK = int(RATE / 10)  # 100ms !!!!THIS IS NOT OUR VARIABLE!!!!
 last_time = time.time()
-prompt = input("write prompt here")
+prompt = 'test'#input("write prompt here")
 words = prompt.split(",")
 print(words)
 
@@ -32,10 +33,6 @@ class correction:
     new_string=""
 
 transcript_corrections = []
-
-
-#SCRIPTPTPTPTPT
-text_file = open("script.txt", "r")
 
 
 class MicrophoneStream(object):
@@ -235,7 +232,7 @@ def athing(inthing):
                 print(d + " this is wrong")
                 break
 def main():
-    global transcript_full, transcript_pending, transcript_corrections
+    global transcript_full, transcript_pending, transcript_corrections, scriptv
     # See http://g.co/cloud/speech/docs/languages
     # for a list of supported languages.
     language_code = 'en-US'  # a BCP-47 language tag
@@ -258,6 +255,13 @@ def main():
 
         # Now, put the transcription responses to use.
         print ("Init.")
+        print("NO")
+        with open("./script.txt", "r") as text_file:
+            sriptv = text_file.readlines()
+            print(sriptv)
+
+        print("daddi")
+
         expected_word = "emeel"
         for cur_response in responses:
             try:
@@ -281,9 +285,3 @@ def main():
             #print(transcript_full+transcript_pending)
 if __name__ == '__main__':
     main()
-
-# Detects speech in the audio file
-response = client.recognize(config, audio)
-
-for result in response.results:
-    print('Transcript: {}'.format(result.alternatives[0].transcript))
