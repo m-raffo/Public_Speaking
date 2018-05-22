49de78c98e04d9f6e2ab25f85513b74f1689a2ff
redforge <ethanjmedei@gmail.com>
2018-05-22 17:13:54 -0400

    Some changers to scripts to not require venv stuff

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 #cd ~/Documents/Projects/PythonMH3/Public_Speaking
 echo "This is a mac only script that requires brew"
 brew install portaudio
 cd "$(dirname "$0")"
 pip3 install virtualenv
-source ./venv/bin/activate
+#source ./venv/bin/activate
 pip3 install -r ./requirements.txt 
-deactivate
+#deactivate
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #cd ~/Documents/Projects/PythonMH3/Public_Speaking
 cd "$(dirname "$0")"
 
-source ./venv/bin/activate
+#source ./venv/bin/activate
 python3 ./user_interface.py
 deactivate
